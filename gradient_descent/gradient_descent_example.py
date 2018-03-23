23f26514e62dfb937da07661de4344f456f87a28
justagist <saifksidhik@gmail.com>
2018-03-23 15:21:51 +0000

    made example as gradientdescent class

--- gradient_descent/gradient_descent_example.py
+++ gradient_descent/gradient_descent_example.py
@@ -1,5 +1,8 @@
 '''
     Gradient Descent Example
+    
+    @author: JustaGist
+    @package: ml_playground
 
 '''
 
@@ -7,90 +10,123 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-global_points = np.genfromtxt("data.csv", delimiter=",")
-learning_rate = 0.0001
-
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 80), ylim=(20,120))
-line, = ax.plot([], [], lw=2)
 
 
-def compute_error(b, m, points):
+class GradientDescent:
+    '''
+        Class for performing Gradient Descent on a set of 2d points by fitting a line on them
+
+    '''
+
+    def __init__(self, learning_rate, points, visualise = False, starting_b = 0.0, starting_m = 0.0, num_iterations = 1000):
+
+        self.learning_rate_ = learning_rate
+        self.points_ = points
+        self.animate_ = visualise
+        self.num_iterations_ = num_iterations
+
+        self.b_ = starting_b # current y-intercept of the fitted line
+        self.m_ = starting_m # current slope of the fitted line
+
+        if self.animate_:
+            self.initialise_plotter_()
+
+
+    def initialise_plotter_(self):
+        '''
+            For matplotlib animation
+        '''
+        self.fig_ = plt.figure()
+        self.ax_ = plt.axes(xlim=(0, 80), ylim=(20,120))
+        self.line_, = self.ax_.plot([], [], lw=2) # ----- an Artist object that should be returned each time in the FuncAnimation function
+
+
+    def compute_error(self):
+        '''
+            Computes the error between the points and the fitted line
+        '''
+        totalError = 0
     
-    totalError = 0
-    
-    for i in range(0, len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
-        totalError += (y - (m * x + b)) ** 2
-    
-    return totalError / float(len(points))
+        for i in range(0, len(self.points_)):
+            x = self.points_[i, 0]
+            y = self.points_[i, 1]
+            totalError += (y - (self.m_ * x + self.b_)) ** 2
+        
+        return totalError / float(len(self.points_))
 
-def step_gradient(b_current, m_current, points, learningRate):
-    
-    b_gradient = 0
-    m_gradient = 0
-    nppoints = np.array(points)
-    N = float(len(nppoints))
-    
-    for i in range(0, len(nppoints)):
-        x = nppoints[i, 0]
-        y = nppoints[i, 1]
-        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
-        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
-    new_b = b_current - (learningRate * b_gradient)
-    new_m = m_current - (learningRate * m_gradient)
-
-    return [new_b, new_m]
-
-def close_event():
-    plt.close()
-
-def animate(i):
-    global global_b
-    global global_m
-    
-    global_b, global_m = step_gradient(global_b, global_m, global_points, learning_rate)
-
-    x = np.linspace(0, 100, 100)
-    y = global_m * x + global_b
-
-    line.set_data(x, y)
-    return line,
+    def init_animation_(self):
+        '''
+            FuncAnimation requirement
+        '''
+        self.line_.set_data([], [])
+        return self.line_,
 
 
-def perform_gradient_descent(starting_b, starting_m, num_iterations):
-    global global_b
-    global global_m
-    global_b = starting_b
-    global_m = starting_m
+    def step_gradient(self):
+        '''
+            Performs one gradient step. Checks the direction of the cost function by finding its derivative and steps towards the direction of lower cost according to the learning rate.
+            self.b_ and self.m_ are updated accordingly.
+        '''
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=num_iterations, interval=200, blit=True)
-    
-    plt.scatter(global_points[:,0], global_points[:,1])
-    plt.show()
+        b_gradient = 0
+        m_gradient = 0
 
-def run():
-    
-    
-    initial_b = 0 # initial y-intercept guess
-    initial_m = 0 # initial slope guess
-    num_iterations = 1000
-    
-    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error(initial_b, initial_m, global_points))
-    
-    print "Running..."
-    
-    perform_gradient_descent(initial_b, initial_m, num_iterations)
-    
-    # print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error(b, m, global_points))
-    # plt.show()
+        nppoints = np.array(self.points_)
+        N = float(len(nppoints))
+        
+        for i in range(0, len(nppoints)):
+            x = nppoints[i, 0]
+            y = nppoints[i, 1]
+            b_gradient += -(2/N) * (y - ((self.m_ * x) + self.b_))
+            m_gradient += -(2/N) * x * (y - ((self.m_ * x) + self.b_))
+        self.b_ = self.b_ - (self.learning_rate_ * b_gradient)
+        self.m_ = self.m_ - (self.learning_rate_ * m_gradient)
 
-def init():
-    line.set_data([], [])
-    return line,
+    def get_line_points_(self):
+        '''
+            Fitting a line for the points according to the current m and b. Returns the Artist object required by the FuncAnimation function
+        '''
+
+        x = np.linspace(0, 100, 2)
+        y = self.m_ * x + self.b_
+
+        self.line_.set_data(x, y)
+        return self.line_
+
+
+    def step_gradient_and_get_line(self, i):
+
+        self.step_gradient()
+        line = self.get_line_points_()
+        return line,
+
+    def run(self):
+        '''
+            Perform gradient descent for the required number of iterations.
+        '''
+        if self.animate_:
+            anim = animation.FuncAnimation(self.fig_, self.step_gradient_and_get_line, init_func=self.init_animation_,
+                               frames=self.num_iterations_, interval=200, blit=True, repeat=False)
+
+            plt.scatter(self.points_[:,0], self.points_[:,1], color='g') # Plotting the points
+            plt.show()
+
+        else:
+
+            for i in range(self.num_iterations_):
+
+                self.step_gradient()
+
 
 
 if __name__ == '__main__':
-    run()
+
+    points = np.genfromtxt("data.csv", delimiter=",")
+
+    gd = GradientDescent(learning_rate = 0.0001, points=points, visualise = True, starting_b = 0, starting_m = 0, num_iterations = 1000)
+
+    print "Initial Error:", gd.compute_error()
+
+    gd.run()
+
+    print "Final Error:", gd.compute_error()
