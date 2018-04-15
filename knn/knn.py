9f3eb36c53a1d8c051ab3765b42e473b0a27d941
justagist <saifksidhik@gmail.com>
2018-04-16 00:54:04 +0100

    edit comments

--- knn/knn.py
+++ knn/knn.py
@@ -9,7 +9,9 @@ class knnClassifier:
     ''' 
         Perform knn classification on the given file, which has num_attributes attributes per data point. 
         The dataset is split into training set and testing set with the raitio split_ratio. 
-        The classifier will classify the test set into k classes.
+        The classifier will classify the test set into separate classes by comparing each instance with the k closest neighbours.
+
+        (Note: The number of classes do not matter)
 
     '''
 
@@ -87,7 +89,7 @@ class knnClassifier:
         distances = []
 
         for x in range(len(self.training_set_)):
-            # ----- find ethe euclidean distance of each data point and add the values to distances
+            # ----- find the euclidean distance of each data point and add the values to distances
             dist = self._euclidean_distance(test_instance, self.training_set_[x])
             distances.append((self.training_set_[x], dist))
 
@@ -148,12 +150,12 @@ if __name__ == '__main__':
     ''' 
         Perform knn classification on the iris.data file, which has 4 attributes per data point. 
         The dataset is split into training set and testing set with the raitio 0.65. 
-        The classifier is supposed to classify the test set into 3 classes (k = 3)
+        The classifier is supposed to classify the test set into 3 classes by comparing the instance with the three (k = 3) closest neighbours.
 
     '''
     classifier = knnClassifier(filename = 'iris.data', num_attributes = 4, split_ratio = 0.65)
 
-    # ----- The iris dataset consists of data of k = 3 different classes.
+    # ----- The iris dataset consists of data of 3 different classes.
     predictions = classifier.classify(k = 3, verbose = True)
 
     print "\nAccuracy of prediction: %f%%\n"%classifier.check_accuracy(predictions)
